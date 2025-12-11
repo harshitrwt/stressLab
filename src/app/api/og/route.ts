cd32bab561dd60bea9f2d70819ca0167400c2534
harshitrwt <harshit.rawat1804@gmail.com>
2025-12-11 13:32:16 +0530

    deployment bug

--- src/app/api/og/route.ts
+++ src/app/api/og/route.ts
@@ -1,28 +1,3 @@
-// import React from "react";
-// import { ImageResponse } from "next/og";
-
-// export const runtime = "edge";
-
-// export function GET() {
-//   return new ImageResponse(
-//     <div>
-//         {
-//         type: "div",
-//         props: {
-//             style: {
-//             fontSize: 48,
-//             background: "black",
-//             color: "white",
-//             width: "100%",
-//             height: "100%",
-//             display: "flex",
-//             alignItems: "center",
-//             justifyContent: "center",
-//             },
-//             children: "Stress Test Summary",
-//         },
-//         },
-//         { width: 1200, height: 630 }
-//     </div>
-//   );
-// }
+export async function GET() {
+  return new Response("Not implemented", { status: 501 });
+}
